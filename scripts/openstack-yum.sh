5bb68b823723e36cdf2ea6800c004c80b2aa4638
kevit <simarg@gmail.com>
2014-05-20 00:55:26 +0300

    fix cloud-user and making a custom scripts

---
+++ scripts/openstack-yum.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+. /tmp/common.sh
+set -x
+
+fi
+
+# install cloud packages
+$yum update
+    $yum install cloud-init cloud-utils cloud-utils-growpart git
+fi
+
+if [ "$OS" == "centos" ] ; then
+    # Change default user to centos and add to wheel
+    # Also make it so that we use proper cloud-init
+    # configuration.
+    sed -ni '/system_info.*/q;p' /etc/cloud/cloud.cfg
+
+fi
+
+$yum erase git
+
+    if [ -e /boot/grub/grub.conf ] ; then
+        sed -i -e 's/rhgb.*/console=ttyS0,115200n8 console=tty0 quiet/' /boot/grub/grub.conf
+        cd /boot
+        ln -s boot .
+    elif [ -e /etc/default/grub ] ; then
+        sed -i -e \
+            's/GRUB_CMDLINE_LINUX=\"\(.*\)/GRUB_CMDLINE_LINUX=\"console=ttyS0,115200n8 console=tty0 quiet \1/g' \
+            /etc/default/grub
+        grub2-mkconfig -o /boot/grub2/grub.cfg
+    fi
+
+# Make sure sudo works properly with openstack
+sed -i "s/^.*requiretty$/Defaults !requiretty/" /etc/sudoers
