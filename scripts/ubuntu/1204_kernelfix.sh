dae404601e91685a341c0dcd4037375d3b5f8f69
kevit <simarg@gmail.com>
2014-07-04 09:45:42 +0300

    fix GRUB_RECORDFAIL_TIMEOUT

--- scripts/ubuntu/1204_kernelfix.sh
+++ scripts/ubuntu/1204_kernelfix.sh
@@ -2,4 +2,15 @@
 apt-get --yes --force-yes remove linux-image-3.11.0-15-generic
 apt-get --yes --force-yes remove linux-image-3.11.0-23-generic
 apt-get --yes --force-yes install linux-image-virtual
+
+if grep '^GRUB_RECORDFAIL_TIMEOUT=' /etc/default/grub ; then
+     echo GOOD: /etc/default/grub
+   else
+        echo FIXING: /etc/default/grub
+        perl -pi.bak -e \
+        's/^(GRUB_TIMEOUT=.*\n)/${1}GRUB_RECORDFAIL_TIMEOUT=2\n/' \
+        /etc/default/grub
+        update-grub
+   fi
 update-grub
+
